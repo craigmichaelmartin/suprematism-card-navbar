e0776ed538c8b1d52c7ac987282f4e3c3627580d
Craig Martin <craigmichaelmartin@gmail.com>
2017-01-06 16:41:24 -0500

    handle routing from outside navbar

--- out-tsc/card-navbar/card-navbar.component.d.ts
+++ out-tsc/card-navbar/card-navbar.component.d.ts
@@ -1,10 +1,12 @@
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import 'rxjs/add/operator/map';
 import { StateManagerService } from '../state-manager.service';
 export declare class CardNavbarComponent implements OnInit {
     private stateManagerService;
+    private router;
     supreSelectedTab: string;
     supreSelectedCard: string;
-    constructor(stateManagerService: StateManagerService);
+    constructor(stateManagerService: StateManagerService, router: Router);
     ngOnInit(): void;
 }
--- out-tsc/card-navbar/card-navbar.component.js
+++ out-tsc/card-navbar/card-navbar.component.js
@@ -9,17 +9,28 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
 require('rxjs/add/operator/map');
 var state_manager_service_1 = require('../state-manager.service');
 var CardNavbarComponent = (function () {
-    function CardNavbarComponent(stateManagerService) {
+    function CardNavbarComponent(stateManagerService, router) {
         this.stateManagerService = stateManagerService;
+        this.router = router;
     }
     CardNavbarComponent.prototype.ngOnInit = function () {
+        var _this = this;
         this.stateManagerService.setModel = {
             selectedTab: this.supreSelectedTab,
             selectedCard: this.supreSelectedCard
         };
+        this.router.events.subscribe(function (event) {
+            if (event instanceof router_1.NavigationStart) {
+                var _a = event.url.split('/'), selectedTab_1 = _a[1], selectedCard_1 = _a[2];
+                _this.stateManagerService.updateModel(function (currentState) {
+                    return Object.assign({}, currentState, { selectedTab: selectedTab_1, selectedCard: selectedCard_1 });
+                });
+            }
+        });
     };
     __decorate([
         core_1.Input(), 
@@ -35,7 +46,7 @@ var CardNavbarComponent = (function () {
             template: require('./card-navbar.component.html'),
             styles: [require('./card-navbar.component.css')]
         }), 
-        __metadata('design:paramtypes', [state_manager_service_1.StateManagerService])
+        __metadata('design:paramtypes', [state_manager_service_1.StateManagerService, router_1.Router])
     ], CardNavbarComponent);
     return CardNavbarComponent;
 }());
